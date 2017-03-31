8438fa9e0efedb9d4f066239dad8361383f1727c
Simon Gee <shandianmao110@vip.qq.com>
2017-03-31 19:10:01 +0800

    下班提交

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace firstapp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public String Welcome(string name,int numTimes=1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name},NumTimes is : {numTimes}");
+        }
+    }
+}
